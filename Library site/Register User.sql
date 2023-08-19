be81fde37dc809e52454c9a19eb503206d6f2f54
Shihabul-Shuvo <shihabul900@gmail.com>
2023-08-19 19:34:21 +0600

    this

--- Library site/Register User.sql
+++ Library site/Register User.sql
@@ -15,7 +15,7 @@ BEGIN
     v_phone_number := '&phone';
     v_name := '&name';
     v_address := '&address';
-	v_membership := 'reader';
+	v_membership := 'Reader';
     v_start_date_lib := TRUNC(SYSDATE); -- Today's date
     v_end_date_lib := TRUNC(ADD_MONTHS(SYSDATE, 1)); -- Same day of next month
 	v_existing_membership := 'none';
@@ -33,24 +33,16 @@ BEGIN
 	END IF;
 	
 	-- If the member exists and is a 'reader', and the new membership is 'customer', update the membership status to 'both'
-    IF v_membership = 'customer' THEN
+	ELSIF v_existing_membership = 'Reader' THEN
         UPDATE Members
-        SET Membership_status = 'both'
+        SET Membership_status = 'Both'
         WHERE Phone_No = v_phone_number;
-			
     COMMIT;
 	DBMS_OUTPUT.PUT_LINE('Updated status as both');
 	
-	ELSIF v_existing_membership = 'customer' AND v_membership = 'reader' THEN
-        UPDATE Members
-        SET Membership_status = 'both'
-        WHERE Phone_No = v_phone_number;
-    COMMIT;
-	DBMS_OUTPUT.PUT_LINE('Already a customer.');
-	
 	ELSE
     -- Insert the values into the Members table
-    INSERT INTO Members VALUES (
+    INSERT INTO Members1 VALUES (
         v_phone_number,
         v_name,
         v_address,
--- conn.sql
+++ conn.sql
@@ -1,11 +1,11 @@
 drop database link site;
 
 create database link site
- connect to system identified by "12345"
+connect to system identified by "12345"
  using '(DESCRIPTION =
        (ADDRESS_LIST =
          (ADDRESS = (PROTOCOL = TCP)
-		 (HOST = 192.168.0.117)
+		 (HOST = 192.168.0.112)
 		 (PORT = 1521))
        )
        (CONNECT_DATA =
