75ccfdc4d4cd9d93803a8760a836633b9e48c291
Kazi Atiqur Rahman <iamatiq7@gmail.com>
2023-09-01 18:55:00 +0600

    done

--- Stationary.sql
+++ Stationary.sql
@@ -1,6 +1,15 @@
+SET SERVEROUTPUT ON
 @"F:\CSE\4.1\CSE 4126 DDSL\BookVault Project\DDS-Lab-Project\Stationary site\fragments.sql"
 @"F:\CSE\4.1\CSE 4126 DDSL\BookVault Project\DDS-Lab-Project\Stationary site\trigger.sql"
+BEGIN
+	DBMS_OUTPUT.PUT_LINE('------------------------------Register User--------------------------------');
+END;
+/
 @"F:\CSE\4.1\CSE 4126 DDSL\BookVault Project\DDS-Lab-Project\Stationary site\Register User.sql"
+BEGIN
+	DBMS_OUTPUT.PUT_LINE('------------------------------Show All Customers--------------------------------');
+END;
+/
 @"F:\CSE\4.1\CSE 4126 DDSL\BookVault Project\DDS-Lab-Project\Stationary site\show all customers.sql"
 @"F:\CSE\4.1\CSE 4126 DDSL\BookVault Project\DDS-Lab-Project\Stationary site\show all members.sql"
 @"F:\CSE\4.1\CSE 4126 DDSL\BookVault Project\DDS-Lab-Project\Stationary site\show available books.sql"
